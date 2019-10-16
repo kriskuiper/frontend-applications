188731055ca228c54e04f626451c44e0621d0288
Kris <kriskuiper74@gmail.com>
2019-10-16 11:40:09 +0200

    feat: set up Hello world example

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,16 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
-
 // Code-splitting is automated for routes
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from '../pages/home';
 
 export default class App extends Component {
-	
+
 	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
+	  *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+	  *	@param {string} event.url	The newly routed URL
+  */
+
 	handleRoute = e => {
 		this.currentUrl = e.url;
 	};
@@ -20,11 +18,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import './style';
+
+const Home = () => (
+	<div>
+		<h1>Hello world</h1>
+		<p>Welcome to my Preact app</p>
+	</div>
+);
+
+export default Home;
