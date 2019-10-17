7e95ab369ea36f81d0979be6f6b885440053f804
Kris <kriskuiper74@gmail.com>
2019-10-17 16:38:09 +0200

    feat(result): set up result component

---
+++ src/components/result/result.js
@@ -0,0 +1,22 @@
+import { h } from 'preact';
+import './style';
+
+const formatDescriptionForResult = (description) => {
+	return description.replace(/<br>/gi, '');
+};
+
+const Result = ({ result }) => {
+	const { img, title, description } = result;
+
+	return (
+		<article class="result">
+			<img src={img} alt={`image of ${title}`} />
+			<h2 class="result__title">{title}</h2>
+			<p class="result__description">
+				{formatDescriptionForResult(description)}
+			</p>
+		</article>
+	);
+};
+
+export default Result;
