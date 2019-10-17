82370b1406fdb36a23c7f9e82f594dedcc975652
Kris <kriskuiper74@gmail.com>
2019-10-17 20:47:34 +0200

    refactor(store): rewrite createStore and export

--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,8 @@ import { createStore, combineReducers } from 'react-redux';
 import { results, expos } from './reducers';
 
 const rootReducer = combineReducers({ results, expos });
+const defaultState = { results: [], expos: [] };
 
-export default createStore(rootReducer, { results: [], expos: [] });
+const store = createStore(rootReducer, defaultState);
+
+export default store;
