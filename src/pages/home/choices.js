e33c1375893fcadba47dac6d55a194b26a2ad49f
Kris <kriskuiper74@gmail.com>
2019-10-17 16:42:10 +0200

    feat(homepage): make choice json

---
+++ src/pages/home/choices.js
@@ -0,0 +1,6 @@
+export default [
+	{ label: 'Kleding', query: 'clothingQuery' },
+	{ label: 'Afrikaanse cultuur', query: 'cultureQuery' },
+	{ label: 'Wapens', query: 'weaponQuery' },
+	{ label: '20e eeuwse juwelen', query: 'jewelleryQuery' }
+];
