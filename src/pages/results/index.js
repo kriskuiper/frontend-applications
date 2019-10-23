c23331ccd80fc587840f3e9f9aabf8eed11524b4
Kris <kriskuiper74@gmail.com>
2019-10-23 15:10:17 +0200

    feat(results): display results

--- src/pages/results/index.js
+++ src/pages/results/index.js
@@ -1,30 +1,24 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import { connect } from 'preact-redux';
 import './style';
 
 import Result from '../../components/result';
 
-const mapStateToProps = (state) => ({
-	expoTitle: state.expoTitle.value,
-	results: state.results
-});
-
-class Results extends Component {
-	state = {
-		currentExpo: { title: this.props.expoTitle, objects: [] }
+const mapStateToProps = (state) => {
+	return {
+		results: state.results,
+		expoTitle: state.expoTitle.value
 	};
+};
 
-	render({ expoTitle, results }, {}) {
-		return (
-			<section>
-				<h2>Resultaten: {expoTitle} </h2>
-				{results.map(result => {
-					<Result result={result} />;
-				})}
-			</section>
-		);
-	}
-}
+const Results = ({ expoTitle, results }) => (
+	<section>
+		<h2>Resultaten:</h2>
+		{results.map(result => (
+			<Result result={result} />
+		))}
+	</section>
+);
 
 
 export default connect(mapStateToProps)(Results);
