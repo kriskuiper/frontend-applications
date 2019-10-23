858ea833866f6328133cafd557ffb333f9ff6f63
Kris <kriskuiper74@gmail.com>
2019-10-23 17:28:17 +0200

    feat(results): add 'add to storage' functionalities

--- src/components/result/index.js
+++ src/components/result/index.js
@@ -1,26 +1,49 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style.css';
 
 import FixedRatio from '../fixed-ratio';
 
 const formatDescriptionForResult = description => description.replace(/<br>/gi, '');
 
-const Result = ({ result }) => {
-	const { img, title, description } = result;
+class Result extends Component {
+	state = { isInCurrentExpo: false }
 
-	return (
-		<article class="result">
-			<FixedRatio height={1} width={1}>
-				<img src={img} alt={`Afbeelding van ${title}`} />
-			</FixedRatio>
-			<h2 class="result__title">{title}</h2>
-			{description ? (
-				<p class="result__description">
-					{formatDescriptionForResult(description)}
-				</p>
-			) : ''}
-		</article>
-	);
-};
+	handleAddToExpo = () => {
+		const { result, onAddToExpo } = this.props;
+
+		this.setState({ isInCurrentExpo: true });
+		onAddToExpo(result);
+	}
+
+	handleDeleteFromExpo = () => {
+		const { result, onDeleteFromExpo } = this.props;
+
+		this.setState({ isInCurrentExpo: false });
+		onDeleteFromExpo(result);
+	}
+
+	render({ result }, { isInCurrentExpo }) {
+		const { img, title, description } = result;
+
+		return (
+			<article class="result">
+				<FixedRatio height={1} width={1}>
+					<img src={img} alt={`Afbeelding van ${title}`} />
+				</FixedRatio>
+				<h2 class="result__title">{title}</h2>
+				{description ? (
+					<p class="result__description">
+						{formatDescriptionForResult(description)}
+					</p>
+				) : ''}
+				{isInCurrentExpo ? (
+					<button onClick={this.handleDeleteFromExpo}>Delete from expo</button>
+				) : (
+					<button onClick={this.handleAddToExpo}>Add to expo</button>
+				)}
+			</article>
+		);
+	}
+}
 
 export default Result;
--- src/pages/results/index.js
+++ src/pages/results/index.js
@@ -1,24 +1,77 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { connect } from 'preact-redux';
 import style from './style.css';
 
+import { addExpoToStorage } from '../../../lib/expo-storage';
+
 import Result from '../../components/result';
 
-const mapStateToProps = (state) => {
-	return {
-		results: state.results,
-		expoTitle: state.expoTitle.value
-	};
-};
+const mapStateToProps = (state) => ({
+	results: state.results,
+	expoTitle: state.expoTitle.value
+});
 
-const Results = ({ expoTitle, results }) => (
-	<section>
-		<h2>Resultaten:</h2>
-		{results.map(result => (
-			<Result result={result} />
-		))}
-	</section>
-);
+class Results extends Component {
+	state = {
+		expoTitle: '',
+		expoObjects: []
+	}
+
+	getCurrentExpo = () => ({
+		title: this.state.expoTitle,
+		objects: this.state.expoObjects
+	})
+
+	handleAddToCurrentExpo = (result) => {
+		const { expoTitle } = this.props;
+		const { expoObjects } = this.state;
+
+		// If there's no title yet, set the title
+		if (!this.state.expoTitle) {
+			this.setState({ expoTitle });
+		}
+
+		this.setState({ expoObjects: [...expoObjects, result ] });
+	}
+
+	handleDeleteFromCurrentExpo = (result) => {
+		const { expoObjects } = this.state;
+		const filteredObjects = expoObjects.filter(expoObject => (
+			expoObject.id !== result.id
+		));
+
+		this.setState({ expoObjects: filteredObjects });
+	}
+
+	handleExposToStorage = () => {
+		const currentExpo = {
+			title: this.state.expoTitle,
+			objects: this.state.expoObjects
+		};
+
+		addExpoToStorage(currentExpo);
+
+		this.setState({ expoObjects: {} });
+		this.setState({ expoTitle: '' });
+	}
+
+	render({ results }) {
+		return (
+			<section>
+				<h2>Resultaten:</h2>
+				<button onClick={this.handleExposToStorage}>Get expo's to localStorage</button>
+				{results.map((result, i) => (
+					<Result
+						key={i}
+						result={result}
+						onAddToExpo={this.handleAddToCurrentExpo}
+						onDeleteFromExpo={this.handleDeleteFromCurrentExpo}
+					/>
+				))}
+			</section>
+		);
+	}
+}
 
 
 export default connect(mapStateToProps)(Results);
