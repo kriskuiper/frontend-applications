38c55505808ab192fded71a2c023fd901d9b3420
Kris <kriskuiper74@gmail.com>
2019-10-28 17:57:15 +0100

    feat(app-icon): add app icon

---
+++ src/components/app-icon/index.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const AppIcon = ({ icon }) => (
+	<img
+		class={style['app-icon']}
+		src={`../../assets/icons/${icon}.svg`}
+		alt=""
+	/>
+);
+
+export default AppIcon;
