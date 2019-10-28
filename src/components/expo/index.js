ab46c86f5572140e3a2c7ea47d520419d7c23d8d
Kris <kriskuiper74@gmail.com>
2019-10-28 20:36:25 +0100

    fix(expo): use results since that's available in the expos

--- src/components/expo/index.js
+++ src/components/expo/index.js
@@ -5,33 +5,31 @@ import FixedRatio from '../fixed-ratio';
 import Result from '../result';
 
 const Expo = ({ expo }) => {
-	const { title, objects } = expo;
+	const { title, results } = expo;
 
-	const firstFourObjects = () => objects.slice(0, 4);
+	const firstFourResults = () => results.slice(0, 4);
 
 	return (
 		<article class={style.expo}>
 			<h2 class={style.expo__title}>{title}</h2>
-			{objects.map(object => (
-				<div>
-					<figure class={style['expo__img-container']}>
-						{firstFourObjects().map(result => (
-							<FixedRatio width={1} height={1}>
-								<img src={result.img} class={style.expo__img} />
-							</FixedRatio>
-						))}
-						<figcaption class="sr-only">
-							Afbeeldingen van de eerste vier werken uit expositie, genaamd: {title}
-						</figcaption>
-					</figure>
-					<details>
-						<summary>Objecten in deze expo</summary>
-						{object.results.map(result => (
-							<Result result={result} />
-						))}
-					</details>
-				</div>
-			))}
+			<div>
+				<figure class={style['expo__img-container']}>
+					{firstFourResults().map(result => (
+						<FixedRatio width={1} height={1}>
+							<img src={result.img} class={style.expo__img} />
+						</FixedRatio>
+					))}
+					<figcaption class="sr-only">
+						Afbeeldingen van de eerste vier werken uit expositie, genaamd: {title}
+					</figcaption>
+				</figure>
+				<details>
+					<summary>Objecten in deze expo</summary>
+					{results.map(result => (
+						<Result result={result} />
+					))}
+				</details>
+			</div>
 		</article>
 	);
 };
