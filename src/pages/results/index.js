62a38a9813b014f51d675bc08f943a3e529f408c
Kris <kriskuiper74@gmail.com>
2019-10-28 13:06:01 +0100

    feat(results): only handle pagination and expo to storage

--- src/pages/results/index.js
+++ src/pages/results/index.js
@@ -5,72 +5,42 @@ import style from './style.css';
 import { addExpoToStorage } from '../../../lib/expo-storage';
 import fetchResultsForQuery from '../../../lib/fetch-results-for-query';
 
-import { setResults } from '../../store/actions';
+import { setResults, clearExpo } from '../../store/actions';
 
 import Result from '../../components/result';
 
-const mapStateToProps = (state) => {
-	return {
-		results: state.results,
-		expoTitle: state.expoTitle.value,
-		currentQuery: state.currentQuery
-	};
-};
+const mapStateToProps = (state) => ({
+	results: state.results,
+	currentExpo: state.currentExpo,
+	currentQuery: state.currentQuery
+});
 
 const mapDispatchToProps = (dispatch) => ({
-	setResults: (results) => { dispatch(setResults(results)); }
+	setResults: (results) => { dispatch(setResults(results)); },
+	clearExpo: () => { dispatch(clearExpo()); }
 });
 
 class Results extends Component {
-	state = {
-		expoTitle: '',
-		expoObjects: [],
-		pageNumber: 1
-	}
-
-	handleAddToCurrentExpo = (result) => {
-		const { expoTitle } = this.props;
-		const { expoObjects } = this.state;
-
-		// If there's no title yet, set the title
-		if (!this.state.expoTitle) {
-			this.setState({ expoTitle });
-		}
-
-		this.setState({ expoObjects: [...expoObjects, result ] });
-	}
-
-	handleDeleteFromCurrentExpo = (result) => {
-		const { expoObjects } = this.state;
-		const filteredObjects = expoObjects.filter(expoObject => (
-			expoObject.id !== result.id
-		));
-
-		this.setState({ expoObjects: filteredObjects });
-	}
+	state = {	pageNumber: 1 }
 
 	handleExposToStorage = () => {
-		const currentExpo = {
-			title: this.state.expoTitle,
-			objects: this.state.expoObjects
-		};
+		const { currentExpo, clearExpo } = this.props;
 
 		addExpoToStorage(currentExpo);
-
-		this.setState({ expoObjects: {} });
-		this.setState({ expoTitle: '' });
+		clearExpo();
 	}
 
 	handlePagination = async () => {
 		const { pageNumber } = this.state;
+		const { setResults, currentQuery } = this.props;
 		const newPageNumber = pageNumber + 1;
 
 		this.setState({ pageNumber: newPageNumber });
 
-		const offset = this.state.pageNumber * 24;
-		const offsetResults = await fetchResultsForQuery('clothingQuery', offset);
+		const offset = pageNumber * 24;
+		const offsetResults = await fetchResultsForQuery(currentQuery, offset);
 
-		this.props.setResults(offsetResults);
+		setResults(offsetResults);
 	}
 
 	render({ results }) {
@@ -82,8 +52,6 @@ class Results extends Component {
 					<Result
 						key={i}
 						result={result}
-						onAddToExpo={this.handleAddToCurrentExpo}
-						onDeleteFromExpo={this.handleDeleteFromCurrentExpo}
 					/>
 				))}
 				<button onClick={this.handlePagination}>Load more</button>
