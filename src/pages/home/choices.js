946fe12f0d1ed0e179fc3da2e0889e6c6dc115c6
Kris <kriskuiper74@gmail.com>
2019-10-28 18:17:32 +0100

    chore: add comment

--- src/pages/home/choices.js
+++ src/pages/home/choices.js
@@ -1,3 +1,8 @@
+/*
+* The query of each choice has to be the same as
+* a query from lib/queries-with-offset
+*/
+
 export default [
 	{ label: 'Kleding', query: 'clothingQuery' },
 	{ label: 'Afrikaanse cultuur', query: 'cultureQuery' },
