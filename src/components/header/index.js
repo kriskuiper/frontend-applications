f1d40d879e8338e01943516f91eb587a3bf1cd5d
Kris <kriskuiper74@gmail.com>
2019-10-30 13:22:34 +0100

    feat(header): style header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,17 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 
 import style from './style.css';
+
+import AppIcon from '../app-icon';
 
 const Header = ({ title }) => (
 	<header class={style.header}>
 		<h1>{title}</h1>
+		<Link href="/expos" class={style.header__link}>
+			<span class="sr-only">Naar expos</span>
+			<AppIcon icon="collections" />
+		</Link>
 	</header>
 );
 
