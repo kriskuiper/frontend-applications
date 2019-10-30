aaa0af054932b5e0db4077393bc5eb754db3a65f
Kris <kriskuiper74@gmail.com>
2019-10-30 12:20:24 +0100

    feat(expo): only render results when details are opened

--- src/components/expo/index.js
+++ src/components/expo/index.js
@@ -1,39 +1,53 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style.css';
 
 import FixedRatio from '../fixed-ratio';
 import Result from '../result';
 
-const Expo = ({ expo }) => {
-	const { title, results } = expo;
+class Expo extends Component {
+	state = { detailsOpen: false }
 
-	const firstFourResults = () => results.slice(0, 4);
+	handleToggle = (event) => {
+		this.setState({ detailsOpen: event.target.open });
+	}
 
-	return (
-		<article class={style.expo}>
-			<h2 class={style.expo__title}>{title}</h2>
-			<div>
-				<figure class={style['expo__img-container']}>
-					{firstFourResults().map(result => (
-						<div class={style['expo__fixed-ratio-container']}>
-							<FixedRatio width={1} height={1}>
-								<img src={result.img} class={style.expo__img} />
-							</FixedRatio>
-						</div>
-					))}
-					<figcaption class="sr-only">
-						Afbeeldingen van de eerste vier werken uit expositie, genaamd: {title}
-					</figcaption>
-				</figure>
-				<details>
-					<summary class={style.expo__summary}>Objecten in deze expo</summary>
-					{results.map(result => (
-						<Result result={result} />
-					))}
-				</details>
-			</div>
-		</article>
-	);
-};
+	renderResults = (detailsOpen) => {
+		const { results } = this.props.expo;
+
+		return detailsOpen
+			? results.map(result => (
+				<Result result={result} />
+			)) : '';
+	}
+
+	render({ expo }, { detailsOpen }) {
+		const { title, results } = expo;
+		const firstFourResults = () => results.slice(0, 4);
+
+		return (
+			<article class={style.expo}>
+				<h2 class={style.expo__title}>{title}</h2>
+				<div>
+					<figure class={style['expo__img-container']}>
+						{firstFourResults().map(result => (
+							<div class={style['expo__fixed-ratio-container']}>
+								<FixedRatio width={1} height={1}>
+									<img src={result.img} class={style.expo__img} />
+								</FixedRatio>
+							</div>
+						))}
+						<figcaption class="sr-only">
+							Afbeeldingen van de eerste vier werken uit expositie, genaamd: {title}
+						</figcaption>
+					</figure>
+					<details onToggle={this.handleToggle}>
+						<summary class={style.expo__summary}>Objecten in deze expo</summary>
+						{this.renderResults(detailsOpen)}
+					</details>
+				</div>
+			</article>
+		);
+	}
+}
 
 export default Expo;
