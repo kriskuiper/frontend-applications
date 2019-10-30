8d8280cf0a0219099969dacabb52dec5af4b02f0
Kris <kriskuiper74@gmail.com>
2019-10-30 11:26:28 +0100

    feat(floatingButton): only show if showFloatingButton is active

--- src/components/floating-button/index.js
+++ src/components/floating-button/index.js
@@ -1,13 +1,15 @@
 import { h } from 'preact';
 import style from './style.css';
 
-const FloatingButton = ({ text, onFloatingButtonClick }) => (
-	<button
-		onClick={onFloatingButtonClick}
-		class={style['floating-button']}
-	>
-		{text}
-	</button>
+const FloatingButton = ({ text, onFloatingButtonClick, showFloatingButton }) => (
+	showFloatingButton ? (
+		<button
+			onClick={onFloatingButtonClick}
+			class={style['floating-button']}
+		>
+			{text}
+		</button>
+	) : ''
 );
 
 export default FloatingButton;
