9b532edd63c543ce813ad9792ebc7d7f6e0afe9c
Kris <kriskuiper74@gmail.com>
2019-10-30 11:48:42 +0100

    feat: delete Navigation(Item) components

--- src/components/navigation-item/index.js
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact';
-import { Link } from 'preact-router';
-
-const NavigationItem = ({ item }) => (
-	<li><Link href={item.link}>{item.label}</Link></li>
-);
-
-export default NavigationItem;
--- src/components/navigation/nav-items.js
+++
@@ -1,4 +0,0 @@
-export default [
-	{ label: 'Home', link: '/' },
-	{ label: 'My expos', link: '/expos' }
-];
